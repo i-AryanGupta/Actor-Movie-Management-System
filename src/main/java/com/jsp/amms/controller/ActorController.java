2b775b5ad5adc20353ffeaac43d6a6ec573dd298
i-AryanGupta <aryangupta6008@gmail.com>
2024-06-16 07:19:10 +0530

    Added actor  Controller

---
+++ src/main/java/com/jsp/amms/controller/ActorController.java
@@ -0,0 +1,14 @@
+package com.jsp.amms.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/version1")
+public class ActorController {
+	
+	
+	
+
+}
